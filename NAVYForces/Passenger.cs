4f24ba90bcdf12f9ad3b04839c8a9670ba607298
archv <archangel852@gmail.com>
2015-12-02 15:08:56 +0300

    Create Passenger.cs

---
+++ NAVYForces/Passenger.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAVYForces
+{
+    interface iPassenger
+    {
+        void SetStatus(int value);
+        int GetStatus();
+        int GetDestination();
+        int GetPosition();
+    }
+
+    class Passenger: iPassenger
+    {
+        private int destination;
+        private int startPosition;
+        private int position;
+        private int status; // -1-idle, 0-on street, 1-in car, 2-arrived
+
+        Passenger(int startPosition, int destination, int status = 0)
+        {
+            this.startPosition = startPosition;
+            this.destination = destination;
+            position = startPosition;
+            this.status = status;  
+            //TODO 1
+        }
+
+        public void SetStatus(int value)
+        {
+            status = value;
+        }
+
+        public int GetStatus() { return status; }
+        public int GetDestination() { return destination; }
+        public int GetPosition() { return position; }
+    }
+}
